dcdb7783232973bfa72b4a7575435f183c545aef
FLCorp <108215519+FLCorp@users.noreply.github.com>
2023-05-01 01:16:27 +0200

    Add files via upload

--- chill.js
+++ chill.js
@@ -1,58 +1,33 @@
-var audioPlayer = document.getElementById("myAudio");
-		var audioSource = document.getElementById("audioSource");
-		var audioTitle = document.getElementById("audioTitle");
-        var audioBtn = document.getElementById("audioBtn");
-        var audio2Btn = document.getElementById("audio2Btn")
-		var defaultSrc = audioSource.getAttribute("src");
-		var defaultTitle = audioTitle.innerHTML;
-        var defaultBtn = audioBtn.innerHTML;
-        var default2Btn = audio2Btn.innerHTML;
-		var audio2Src = "https://youtu.be/GdzrrWA8e7A";
-		var audio2Title = "Zelda & Chill - GameChops";
-		var audio3Src = "https://youtu.be/p9a-AQQJ8Aw";
-		var audio3Title = "Mario & Chill - GameChops";
-
-		function changeAudio(audioNumber) {
-			var newSrc, newTitle;
-			if (audioNumber === 2) {
-				if (audioSource.getAttribute("src") === defaultSrc) {
-					newSrc = audio2Src;
-					newTitle = audio2Title;
-                    newBtn = "Chilltendo - GameChops";
-                    new2Btn = "Mario & Chill - GameChops";
-				} else if (audioSource.getAttribute("src") === audio2Src) {
-					newSrc = defaultSrc;
-					newTitle = defaultTitle;
-                    newBtn = "Zelda & Chill - GameChops";
-                    new2Btn = "Mario & Chill - GameChops";
-				} else {
-					newSrc = audio2Src;
-					newTitle = audio2Title;
-                    newBtn = "Mario & Chill - GameChops";
-                    new2Btn = "Chilltendo - GameChops";
-				}
-			} else if (audioNumber === 3) {
-				if (audioSource.getAttribute("src") === defaultSrc) {
-					newSrc = audio3Src;
-					newTitle = audio3Title;
-                    newBtn = "Zelda & Chill - GameChops";
-                    new2Btn = "Chilltendo - GameChops";
-				} else if (audioSource.getAttribute("src") === audio3Src) {
-					newSrc = defaultSrc;
-					newTitle = defaultTitle;
-                    newBtn = "Zelda & Chill - GameChops";
-                    new2Btn = "Mario & Chill - GameChops";
-				} else {
-					newSrc = audio3Src;
-					newTitle = audio3Title;
-                    newBtn = "Zelda & Chill - GameChops";
-                    new2Btn = "Chilltendo - GameChops";
-				}
-			}
-			audioSource.setAttribute("src", newSrc);
-			audioTitle.innerHTML = newTitle;
-            audioBtn.innerHTML = newBtn;
-            audio2Btn.innerHTML = new2Btn;
-			audioPlayer.load();
-			audioPlayer.play();
-		}
+var player;
+      var defaultVideoId = 'c5V-qL1HRho'; // URL de la vidéo par défaut
+      var currentVideoId = defaultVideoId;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '0',
+          width: '0',
+          videoId: defaultVideoId,
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+      function onPlayerReady(event) {
+        event.target.setVolume(10); // Définit le volume initial à 10%
+        event.target.playVideo(); // Lance la lecture automatique
+      }
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          done = true;
+        }
+      }
+      function changeVideo(videoId) {
+        currentVideoId = videoId;
+        player.loadVideoById(currentVideoId); // Charge une nouvelle vidéo
+        player.playVideo(); // Lance la lecture automatique
+      }
+      function resetVideo() {
+        currentVideoId = defaultVideoId;
+        player.loadVideoById(currentVideoId); // Charge la vidéo par défaut
+        player.playVideo(); // Lance la lecture automatique
+      }
